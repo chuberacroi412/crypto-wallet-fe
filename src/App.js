b1b39bf618507bd9071d5f9adc5b24601a62b04f
chuberacroi412 <lamanhtuan1299@gmail.com>
2022-04-15 20:12:15 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Logo from './components/logo/Logo';
+
+function App() {
+  return (
+    <div>
+      <Logo/>
+      <p>some text</p>
+    </div>
+  );
+}
+
+export default App;
