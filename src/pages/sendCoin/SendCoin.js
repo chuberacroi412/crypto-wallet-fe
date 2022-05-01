87618d1c113705b75ee6352cb18a19e03d76f8c6
chuberacroi412 <lamanhtuan1299@gmail.com>
2022-05-02 00:10:09 +0700

    send coin static page

--- src/pages/sendCoin/SendCoin.js
+++ src/pages/sendCoin/SendCoin.js
@@ -7,7 +7,29 @@ export default function SendCoin() {
         <>
             <Nav/>
             <div className='send'>
-                <Menu send={true}/>
+                <Menu className='send-left-content' send={true} />
+                <div className='send-content'>
+                    <div className='send-dialog'>
+                        <div className='send-dialog-item'>
+                            <div>
+                                <label htmlFor='type'>Type</label>
+                                <input id='type' value='ETH - Ethereum' readOnly />
+                            </div>
+                            <div>
+                                <label htmlFor='amount'>Amount</label>
+                                <input id='amount' type='number' min='0' placeholder='1223'/>
+                            </div>
+                        </div>
+                        <div className='send-dialog-address'>
+                            <label htmlFor='address'>To Address</label>
+                            <input id='address' placeholder='0xDECAF9CD2367cdbb726E904cD6397eDFcAe6068D' />
+                        </div>
+                        <div>
+                            <button className='send-button send-button-clear'>Clear</button>
+                            <button className='send-button send-button-send'>Send</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
